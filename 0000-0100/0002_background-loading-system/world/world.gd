ab334e954bee0ba830e4f482cf21614ccedcf3ba
shopkeeper <734212732@qq.com>
2025-03-08 15:12:29 +0800

    add: 0003_top-down-player-movement-with-animations fix res path

--- 0000-0100/0002_background-loading-system/world/world.gd
+++ 0000-0100/0002_background-loading-system/world/world.gd
@@ -4,4 +4,4 @@ extends Node2D
 
 
 func _on_button_pressed() -> void:
-	LoadManager.load_scene("res://0000_0100/0001_parallax-variation/scenes/parallax_variation.tscn")
+	LoadManager.load_scene("res://0000-0100/0001_parallax-variation/scenes/parallax_variation.tscn")
--- screenshots/results-show_0001-0100.md
+++ screenshots/results-show_0001-0100.md
@@ -7,3 +7,6 @@
 ### 0002_background-loading-system
 
 ![0002_background-loading-system](0002.gif)
+
+### 0003_top-down-player-movement-with-animations
+![0003_top-down-player-movement-with-animations](0003.gif)
